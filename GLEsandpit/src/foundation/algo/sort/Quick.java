539a4622b3ee74f82b0b60803508f328c01544c2
User <User@L-Lat5520-004>
2015-01-03 18:28:51 +0000

    update

--- GLEsandpit/src/foundation/algo/sort/Quick.java
+++ GLEsandpit/src/foundation/algo/sort/Quick.java
@@ -1,8 +1,11 @@
 package foundation.algo.sort;
 
+import foundation.inheritance.Human;
+
 public class Quick {
 
 	private int array[];
+	private Human humanarray[];
     private int length;
  
     public void sort(int[] inputArr) {
@@ -15,6 +18,15 @@ public class Quick {
         quickSort(0, length - 1);
     }
  
+   
+ 
+    private void exchangeNumbers(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    
+    
     private void quickSort(int lowerIndex, int higherIndex) {
          
         int i = lowerIndex;
@@ -48,13 +60,62 @@ public class Quick {
         if (i < higherIndex)
             quickSort(i, higherIndex);
     }
- 
-    private void exchangeNumbers(int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
      
+    
+    
+    
+ public void sort(Human[] inputArr) {
+        
+        if (inputArr == null || inputArr.length == 0) {
+            return;
+        }
+        this.humanarray = inputArr;
+        length = inputArr.length;
+        quickSortHuman(0, length - 1);
+    }
+
+ 
+    private void exchangeHuman(int i, int j) {
+        Human temp = humanarray[i];
+        humanarray[i] = humanarray[j];
+        humanarray[j] = temp;
+    }
+    
+    private void quickSortHuman(int lowerIndex, int higherIndex) {
+        
+        int i = lowerIndex;
+        int j = higherIndex;
+        // calculate pivot number, I am taking pivot as middle index number
+        Human pivot = humanarray[lowerIndex+(higherIndex-lowerIndex)/2];
+        // Divide into two arrays
+        while (i <= j) {
+            /**
+             * In each iteration, we will identify a number from left side which
+             * is greater then the pivot value, and also we will identify a number
+             * from right side which is less then the pivot value. Once the search
+             * is done, then we exchange both numbers.
+             */
+            while (humanarray[i].getYearage() < pivot.getYearage()) {
+                i++;
+            }
+            while (humanarray[j].getYearage() > pivot.getYearage()) {
+                j--;
+            }
+            if (i <= j) {
+            	exchangeHuman(i, j);
+                //move index to next position on both sides
+                i++;
+                j--;
+            }
+        }
+        // call quickSort() method recursively
+        if (lowerIndex < j)
+        	quickSortHuman(lowerIndex, j);
+        if (i < higherIndex)
+        	quickSortHuman(i, higherIndex);
+    }
+    
+    
     public static void main(String a[]){
          
     	Quick sorter = new Quick();
--- GLEsandpit/src/foundation/algo/sort/test/AssertTests.java
+++ GLEsandpit/src/foundation/algo/sort/test/AssertTests.java
@@ -2,18 +2,56 @@ package foundation.algo.sort.test;
 
 import org.junit.Test;
 
+import foundation.algo.sort.Quick;
+import foundation.inheritance.Human;
+
 public class AssertTests {
 	
 	  @Test
 	  public void testAssertArrayEquals() {
 	    
+		/* Human[] team =  new Human[5];
+		Human[] team2 =  new Human[5];
 		
+		team[0] = new Human();
+		team[1] = new Human();
+		team[2] = new Human();
+		team[3] = new Human();
+		team[4] = new Human();
+		
+		team2[0] = new Human();
+		team2[1] = new Human();
+		team2[2] = new Human();
+		team2[3] = new Human();
+		team2[4] = new Human();
+		
+		team[0].setYearage(35);
+		team[1].setYearage(54);
+		team[2].setYearage(39);
+		team[3].setYearage(40);
+		team[4].setYearage(20);
+		
+		
+		team2[0].setYearage(35);
+		team2[1].setYearage(54);
+		team2[2].setYearage(39);
+		team2[3].setYearage(40);
+		team2[4].setYearage(20); */
+		
+		Quick sorter = new Quick();
+        int[] input = {24,2,45,20,56,75,2,56,99,53,12};
+        int[] input2 = {24,2,45,20,56,75,2,56,99,53,12};
+        
+        sorter.sort(input);
+        //sorter.sort(input2);
 		  
-		byte[] expected = "trial".getBytes();
-	    byte[] actual = "trial".getBytes();
+	//	byte[] expected = "trial".getBytes();
+	//    byte[] actual = "trial".getBytes();
 	    
 	    
-	    org.junit.Assert.assertArrayEquals("failure - byte arrays not same", expected, actual);
+	    //org.junit.Assert.assertArrayEquals("failure - byte arrays not same", expected, actual);
+	    
+	    org.junit.Assert.assertArrayEquals("failure - human arrays not same", input, input2);
 	  }
 	  
 	  
